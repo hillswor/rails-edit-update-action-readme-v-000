107fdc6318b353633f7ce9c61b3afdbdd3b4decc
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-01-07 22:22:32 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,8 +24,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    @article.update(title: params[:title], description: params[:description])
-    redirect_to article_path(@article)
+    raise params.inspect
   end
 end
